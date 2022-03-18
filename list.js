5f33fb405df9eb6d9c8929dd6abe1c9e68ad4dd7
d-okalibe <99320254+d-okalibe@users.noreply.github.com>
2022-03-18 14:24:12 +0100

    subject lists

---
+++ list.js
@@ -0,0 +1,69 @@
+let products = [
+  {
+    id: 1001,
+    title: "Physics",
+    description: "Learn physical laws and formulas .",
+    price: 250.0,
+    location: "Ogba",
+    image:
+      "https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60-fullsize.png",
+    availableInventory: 10,
+    rating: 2,
+  },
+  {
+    id: 1002,
+    title: "French",
+    description: "Explore the beauty of the French Language.",
+    price: 350.99,
+    location: "Ketu",
+    image:
+      "https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60",
+    availableInventory: 17,
+    rating: 3,
+  },
+  {
+  id: 1001,
+    title: "English",
+    description: "Speak Better English Today.",
+    price: 250.0,
+    location: "Mushin",
+    image:
+      "https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60-fullsize.png",
+    availableInventory: 10,
+    rating: 2,
+  },
+  {
+    id: 1003,
+    title: "Sex Education",
+    description: "Know more about your body.",
+    price: 150.0,
+    location: "Ikeja",
+    image:
+      "https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60.jpg",
+    availableInventory: 200,
+    rating: 5,
+  },
+  {
+    id: 1004,
+    title: "Chemistry",
+    description: "your periodic table is important.",
+    price: 300,
+    location: "Maryland",
+    image:
+      "https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60/cat-house.jpg",
+    availableInventory: 11,
+    rating: 5,
+  },
+  {
+    id: 1005,
+    title: "Mathematics",
+    description: "Get your number and calculations right.",
+    price: 350,
+    location: "Oshodi",
+    image:
+      "https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60-pointer.jpg",
+    availableInventory: 25,
+    rating: 4,
+  },
+];
+  
